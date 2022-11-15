e7ab84e0236f3e991a198511417c642603f26e5e
tfotr241290 <franco_9024@hotmail.com>
2022-11-15 12:35:29 -0600

    completed assignment

--- javascript/weatherapi.js
+++ javascript/weatherapi.js
@@ -12,12 +12,30 @@ const requestData = async(url)=>{
     if(response.ok === true){
         const data = await response.json();
         console.log(data);
+        displayResults(data);
     }
     else{
-        console.log("The requested url is invalid please check your URL and API key to make sure they are correct.")
+        console.log("The requested url is invalid please check your URL and API key to make sure they are correct.");
     }
+};
 
+function capFirstLetter(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const displayResults = (weatherData)=>{
+    let temperature = parseInt(weatherData.main.temp);
+    currentTemp.innerHTML = `<strong>${temperature}</strong>`;
+
+    const iconImg = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`
+    const descript = weatherData.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconImg);
+    weatherIcon.setAttribute('alt','weather-icon');
+
+    captionDesc.innerText = capFirstLetter(descript);
 
 }
+
 
 requestData(url);
